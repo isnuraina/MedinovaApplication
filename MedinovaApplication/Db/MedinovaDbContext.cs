2584bd41ede9b04e3a7273237cbff2537b419cad
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2025-12-01 17:27:48 +0400

    Add project files.

---
+++ MedinovaApplication/Db/MedinovaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MedinovaApplication.Db
+{
+    public class MedinovaDbContext:DbContext
+    {
+        public MedinovaDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MedinovaApplication/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace MedinovaApp.Models
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+    }
+}
