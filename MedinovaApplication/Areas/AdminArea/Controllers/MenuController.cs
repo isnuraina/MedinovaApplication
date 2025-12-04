156191c1fd1890247b73f503256cf0af204f7244
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2025-12-04 17:50:35 +0400

    some changes is added

--- MedinovaApplication/Areas/AdminArea/Controllers/MenuController.cs
+++ MedinovaApplication/Areas/AdminArea/Controllers/MenuController.cs
@@ -3,6 +3,7 @@ using MedinovaApplication.Db;
 using MedinovaApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 namespace MedinovaApplication.Areas.AdminArea.Controllers
 {
     [Area("AdminArea")]
@@ -29,16 +30,24 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
                     Haschildren = _context.MenuItems.Any(c => c.ParentId == m.Id),
                 })
                 .AsQueryable();
-          
+
             if (sub is not null && sub != 0)
             {
                 menuItems = menuItems.Where(m => m.ParentId == sub);
+
+                var parentMenu = await _context.MenuItems.FindAsync(sub);
+                if (parentMenu != null)
+                {
+                    ViewBag.ParentName = parentMenu.Name;
+                    ViewBag.ParentId = parentMenu.Id;
+                }
             }
             else
             {
-                menuItems = menuItems.Where(x => x.ParentId == 0 || x.ParentId == null)
-                .OrderBy(m => m.ParentId ?? 0)
-                .ThenBy(m => m.OrderIndex);
+                menuItems = menuItems
+                    .Where(x => x.ParentId == 0 || x.ParentId == null)
+                    .OrderBy(m => m.ParentId ?? 0)
+                    .ThenBy(m => m.OrderIndex);
             }
 
             var result = await menuItems.ToListAsync();
@@ -50,6 +59,7 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
 
             return View(result);
         }
+
         public async Task<IActionResult> Children(int id)
         {
             var parentMenu = await _context.MenuItems
@@ -66,19 +76,22 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
 
             return View(parentMenu.Children);
         }
+
         public async Task<IActionResult> Create(int? parentId)
         {
             var vm = new MenuItemVM();
-            if(parentId.HasValue && parentId.Value>0)
+
+            if (parentId.HasValue && parentId.Value > 0)
             {
                 vm.ParentId = parentId.Value;
-                var parent=await _context.MenuItems.FindAsync(parentId.Value);
+                var parent = await _context.MenuItems.FindAsync(parentId.Value);
                 if (parent != null)
                 {
                     ViewBag.ParentName = parent.Name;
                 }
             }
-            ViewBag.ParentMenus=await _context.MenuItems
+
+            ViewBag.ParentMenus = await _context.MenuItems
                 .Where(m => m.ParentId == null || m.ParentId == 0)
                 .OrderBy(m => m.OrderIndex)
                 .Select(m => new MenuItemVM
@@ -87,8 +100,10 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
                     Name = m.Name
                 })
                 .ToListAsync();
+
             return View(vm);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(MenuItemVM vm)
@@ -98,8 +113,8 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
                 var menuItem = new MenuItem
                 {
                     Name = vm.Name,
-                    Url = vm.Url,
-                    ParentId = vm.ParentId > 0 ? vm.ParentId : null,
+                    Url = string.IsNullOrWhiteSpace(vm.Url) ? null : vm.Url,
+                    ParentId = (vm.ParentId.HasValue && vm.ParentId.Value > 0) ? vm.ParentId : null,
                     OrderIndex = vm.OrderIndex,
                     IsActive = vm.IsActive,
                     CreatedDate = DateTime.Now
@@ -110,7 +125,7 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
 
                 TempData["Success"] = "Menyu uğurla əlavə edildi!";
 
-                if (vm.ParentId > 0)
+                if (vm.ParentId.HasValue && vm.ParentId.Value > 0)
                 {
                     return RedirectToAction(nameof(Index), new { sub = vm.ParentId });
                 }
@@ -130,6 +145,126 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
 
             return View(vm);
         }
+
+        // GET: Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var menuItem = await _context.MenuItems.FindAsync(id);
+
+            if (menuItem is null)
+            {
+                return NotFound();
+            }
+
+            var vm = new MenuItemVM
+            {
+                Id = menuItem.Id,
+                Name = menuItem.Name,
+                Url = menuItem.Url,
+                ParentId = menuItem.ParentId, 
+                OrderIndex = menuItem.OrderIndex,
+                IsActive = menuItem.IsActive,
+                CreatedDate = menuItem.CreatedDate
+            };
+
+            ViewBag.ParentMenus = await _context.MenuItems
+                .Where(m => (m.ParentId == null || m.ParentId == 0) && m.Id != id)
+                .OrderBy(m => m.OrderIndex)
+                .Select(m => new MenuItemVM
+                {
+                    Id = m.Id,
+                    Name = m.Name
+                })
+                .ToListAsync();
+
+            if (menuItem.ParentId.HasValue && menuItem.ParentId.Value > 0)
+            {
+                var parent = await _context.MenuItems.FindAsync(menuItem.ParentId.Value);
+                if (parent != null)
+                {
+                    ViewBag.ParentName = parent.Name;
+                }
+            }
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, MenuItemVM vm)
+        {
+            if (id != vm.Id)
+            {
+                TempData["Error"] = "ID uyğunsuzluğu!";
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.Remove("RowNumber");
+            ModelState.Remove("Haschildren");
+            ModelState.Remove("ParentName");
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var menuItem = await _context.MenuItems.FindAsync(id);
+
+                    if (menuItem == null)
+                    {
+                        TempData["Error"] = "Menyu tapılmadı!";
+                        return RedirectToAction(nameof(Index));
+                    }
+
+                    menuItem.Name = vm.Name;
+                    menuItem.Url = string.IsNullOrWhiteSpace(vm.Url) ? null : vm.Url;
+                    menuItem.ParentId = (vm.ParentId.HasValue && vm.ParentId.Value > 0) ? vm.ParentId : null;
+                    menuItem.OrderIndex = vm.OrderIndex;
+                    menuItem.IsActive = vm.IsActive;
+
+                    _context.Update(menuItem);
+                    await _context.SaveChangesAsync();
+
+                    TempData["Success"] = "Menyu uğurla yeniləndi!";
+
+                    if (vm.ParentId.HasValue && vm.ParentId.Value > 0)
+                    {
+                        return RedirectToAction(nameof(Index), new { sub = vm.ParentId });
+                    }
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuItemExists(vm.Id))
+                    {
+                        TempData["Error"] = "Menyu artıq mövcud deyil!";
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            ViewBag.ParentMenus = await _context.MenuItems
+                .Where(m => (m.ParentId == null || m.ParentId == 0) && m.Id != id)
+                .OrderBy(m => m.OrderIndex)
+                .Select(m => new MenuItemVM
+                {
+                    Id = m.Id,
+                    Name = m.Name
+                })
+                .ToListAsync();
+
+            return View(vm);
+        }
+
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -142,7 +277,6 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
             if (menuItem == null)
                 return NotFound();
 
-           
             var vm = new MenuItemVM
             {
                 Id = menuItem.Id,
@@ -155,7 +289,6 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
                 Haschildren = await _context.MenuItems.AnyAsync(c => c.ParentId == id)
             };
 
-           
             if (menuItem.Parent != null)
             {
                 ViewBag.ParentName = menuItem.Parent.Name;
@@ -200,6 +333,7 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
         [HttpPost]
         public async Task<IActionResult> ToggleActive(int id)
         {
@@ -218,6 +352,5 @@ namespace MedinovaApplication.Areas.AdminArea.Controllers
         {
             return _context.MenuItems.Any(e => e.Id == id);
         }
-
     }
 }
--- MedinovaApplication/Areas/AdminArea/Models/VMs/MenuItemVM.cs
+++ MedinovaApplication/Areas/AdminArea/Models/VMs/MenuItemVM.cs
@@ -1,15 +1,37 @@
-﻿namespace MedinovaApplication.Areas.AdminArea.Models.VMs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedinovaApplication.Areas.AdminArea.Models.VMs
 {
     public class MenuItemVM
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Menyu adı mütləqdir")]
+        [StringLength(100, ErrorMessage = "Menyu adı maksimum 100 simvol ola bilər")]
+        [Display(Name = "Menyu Adı")]
         public string Name { get; set; }
+
+        [StringLength(200, ErrorMessage = "URL maksimum 200 simvol ola bilər")]
+        [Display(Name = "URL")]
         public string? Url { get; set; }
-        public int RowNumber { get; set; } 
+
+        public int RowNumber { get; set; }
+
+        [Display(Name = "Parent Menyu")]
         public int? ParentId { get; set; }
+
+        [Required(ErrorMessage = "Sıra nömrəsi mütləqdir")]
+        [Display(Name = "Sıra")]
         public int OrderIndex { get; set; }
+
+        [Display(Name = "Aktiv")]
         public bool IsActive { get; set; } = true;
+
         public bool Haschildren { get; set; }
+
+        [Display(Name = "Yaranma Tarixi")]
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public string? ParentName { get; set; }
     }
 }
